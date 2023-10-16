28f3b816cb7f5779d0e8c70e5f29820fbb863eca
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-16 17:40:23 +0200

    Soluzione di Marco

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-
-// let toBuyList = [];
-// let itemToBuy = [prompt('Quali prodotti vuoi acquistare?')];
-
-// toBuyList = itemToBuy.push([0]);
-// console.log(toBuyList);
-// console.log(itemToBuy);
-
 let list = [];
 let item ='';
 
