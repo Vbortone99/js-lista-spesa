3e608ebf157ff5a6d6a1bf225eefd57efbdb3e66
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-16 17:39:59 +0200

    Soluzione di Marco

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 
-let toBuyList = [];
-let itemToBuy = [prompt('Quali prodotti vuoi acquistare?')];
+// let toBuyList = [];
+// let itemToBuy = [prompt('Quali prodotti vuoi acquistare?')];
 
-toBuyList = itemToBuy.push([0]);
-console.log(toBuyList);
-console.log(itemToBuy);
+// toBuyList = itemToBuy.push([0]);
+// console.log(toBuyList);
+// console.log(itemToBuy);
+
+let list = [];
+let item ='';
+
+while(item !== 'stop' && item !== '') {
+   item = prompt('aggiungi alla lista');
+   if(item !== 'stop'){
+       list.push(item);
+   }
+};
+
+const listEl = document.getElementById('list');
+for(i = 0; i < list.length; i++){
+    list[i];
+    const listItemEl = document.createElement('li');
+    listItemEl.innerHTML = list[i];
+    listEl.appendChild(listItemEl);
+};
