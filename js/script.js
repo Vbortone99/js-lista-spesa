74fc04503c5cfcf56c2fd54927a662b0495bfc14
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-16 17:46:01 +0200

    complete

--- js/script.js
+++ js/script.js
@@ -3,14 +3,16 @@ let item ='';
 
 while(item !== 'stop'){
    item = prompt('aggiungi alla lista');
-   if(item !== 'stop' && item !==  ''){
+   item = item.trim();
+   item = item.toLocaleLowerCase();
+
+   if(item !== 'stop' && item !== ''){
        list.push(item);
    }
 };
 
 const listEl = document.getElementById('list');
 for(i = 0; i < list.length; i++){
-    list[i];
     const listItemEl = document.createElement('li');
     listItemEl.innerHTML = list[i];
     listEl.appendChild(listItemEl);
